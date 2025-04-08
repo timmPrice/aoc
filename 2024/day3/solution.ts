4ee3653e8f033ade9760ce87450c847301b91917
timmPrice <timmPrice6@gmail.com>
2025-04-07 22:43:09 -0400

    finishing day 3 aoc 2024

--- 2024/day3/solution.ts
+++ 2024/day3/solution.ts
@@ -3,21 +3,41 @@
 //      Maybe rust tommorow?
 // ================================
 
-function parseMult(aList: string[]): number {
+function parseMult(aMult: string): number {
     // worth knowing more about capture groups and .map functions
     const pattern = /(-?\d+),(-?\d+)/ 
     let results: number[] = [];
     let total: number = 0;
-    for (let i = 0; i < aList.length; i++){
-        const match = aList[i].match(pattern)
-        total += Number(match[1]) * Number(match[2]);
-    }
-
+    const match = aMult.match(pattern)
+    total += Number(match[1]) * Number(match[2]);
     return total
 }
 
+function parseDoDont(aList: string[]): number{
+    const mulPat = /mul\(-?\d+,-?\d+\)/g;
+    const doPat = /do\(\)/g;
+    const dontPat = /don\'t\(\)/g;
+    let run = true;
+    let total = 0;
+    for (let i = 0; i < aList.length; i++){
+        if (aList[i].match(doPat)) {
+            run = true; 
+            continue; 
+        } else if(aList[i].match(dontPat)){
+            run = false;
+            continue;
+        } else if (aList[i].match(mulPat)){
+            if (run == true) {
+                total += parseMult(aList[i]) 
+            }  
+        }
+    }
+
+    return total;
+}
+
 function matchPatterns(input: string): string[] {
-    const pattern = /mul\(-?\d+,-?\d+\)/g;
+    const pattern = /mul\(-?\d+,-?\d+\)|do\(\)|don\'t\(\)/g;
     return Array.from(input.matchAll(pattern)).map(m => m[0]);
 }
 
@@ -25,9 +45,12 @@ async function main(): void {
     const file = Bun.file("./input.txt");
     let input: string = await file.text();
     let matches: string = matchPatterns(input);
-    let multmatch: number = parseMult(matches); 
-    console.log(matches)
-    console.log(multmatch)
+    //console.log(matches)
+    //let multmatch: number = parseMult(matches); 
+    //console.log("section 1 answer: ", multmatch)
+
+    let total: number = parseDoDont(matches);
+    console.log(total)
 }
 
 main()
