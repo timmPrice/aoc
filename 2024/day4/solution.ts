7fcf8a7a57a68dbcf1aa6592a2ec1da8d1744925
timmPrice <timmPrice6@gmail.com>
2025-04-11 01:33:05 -0400

    still really struggling with day4... I think I'm almost there

--- 2024/day4/solution.ts
+++ 2024/day4/solution.ts
@@ -8,25 +8,151 @@ function toArray(aString: string): string[][] {
     return aList;
 }
 
-function printTwoArray(array: string[][]) {
+function check(array: string[][], target): number{
+    let total = 0;
+    const reversed = [...target].reverse();
+   
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array[i].length; j++) {
-            console.log(i, j, ": ",array[i][j])
+            if (array[i][j] === target[0]) {
+                //if (checkRow(array, i, j, target) == true){
+                //    total++;
+                //}
+                //if (checkCol(array, i, j, target) == true){
+                //    total++;
+                //}
+                //if (checkNegSlope(array, i, j, target) == true){
+                //    total++;
+                //}
+                console.log(array[i][j], "starting TARGET")
+                if (checkPosSlope(array, i, j, target) == true){
+                    total++;
+                }
+            } else if (array[i][j] === reversed[0]){
+                //if (checkRow(array, i, j, reversed) == true){
+                //    total++;
+                //}
+                //if (checkCol(array, i, j, reversed) == true){
+                //    total++;
+                //}
+                //if (checkNegSlope(array, i, j, reversed) == true){
+                //    total++;
+                //}
+                console.log(array[i][j], "starting REVERSED")
+                if (checkPosSlope(array, i, j, reversed) == true){
+                    total++;
+                }
+            }
         }
     }
-}
-
-function checkColumn(): number {
     
+    return total;
 }
 
+function checkRow(array: string[][], x: number, y: number, target: string[]): boolean {
+    let run = 0;
+    for (let i = y; i < array[x].length - 1; i++){
+        if (array[x][i] == target[run]){
+            run++;
+            continue;
+        } else {
+            break;
+        }
+    }
+    if (run === target.length) {
+        return true
+    }
+    
+    return false;
+}
+
+function checkCol(array: string[][], x: number, y: number, target: string[]): boolean {
+    console.log("COLS ==================")
+    let run = 0;
+    for (let i = x; i < array.length - 1; i++){
+        console.log("array: ", array[x][i], "/ x: ", x, "/ y: ", y)
+        if (array[i][y] == target[run]){
+            run++;
+            console.log("if-was-true")
+            continue;
+        } else {
+            break; 
+        }
+    }
+    console.log("end: ", run)
+    if (run === target.length) {
+        return true; 
+    }
+
+    return false;
+}
+
+
+function checkNegSlope(array: string[][], x: number, y: number, target: string[]): boolean{
+    console.log("NEGSLOPE ==================")
+    let run = 0;
+    for (let i = 0; i < array?.length && i < array[x]?.length; i++) {
+        console.log("/ ARRAY: ", array[x+i][y+i])
+        console.log("/ x: ", x, "/ y: ", y)
+        if (
+            x + i < array.length &&
+            y + i < array[x].length &&
+            array[x+i][y+i] === target[run]
+        ){
+            console.log("if-was-true")
+            run++;
+            continue;
+        } else {
+            break;
+        } 
+    }
+    console.log("end: ", run)
+    if (run == target.length){
+        return true;
+    }
+
+    return false;
+}
+
+
+function checkPosSlope(array: string[][], x: number, y: number, target: string[]): boolean{
+    console.log("POSSLOPE ==================")
+    let run = 0;
+    for (let i = 0; i < array?.length && i < array[x]?.length; i++) {
+        if (
+            x + i < array.length &&
+            y - i >= 0){
+
+        console.log("/ ARRAY: ", array[x+i][y-i])
+            }
+        console.log("/ x: ", x, "/ y: ", y)
+        if (
+            x + i < array.length &&
+            y - i >= 0 &&
+            array[x+i][y-i] === target[run]
+        ){
+            console.log("if-was-true")
+            run++;
+            continue;
+        } else {
+            break;
+        } 
+    }
+    
+    console.log("end: ", run)
+    if (run == target.length){
+        return true;
+    }
+
+    return false;
+}
 
 async function main() {
-    const file = Bun.file("input.txt")
+    const file = Bun.file("example.txt")
     let input: string = await file.text();
-    let myArray: string[][] = toArray(input);
-    printTwoArray(myArray);
+    const myArray: string[][] = toArray(input);
+    const target = ["X","M","A","S"];
+    console.log(check(myArray, target))
 }
-
 
 main()
