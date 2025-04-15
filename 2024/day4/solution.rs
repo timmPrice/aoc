b5141cc852b23863609956bd24a8785a15ea809b
timmPrice <timmPrice6@gmail.com>
2025-04-15 15:41:22 -0400

    making day 4 nicer with slices of arrays for variable target size

--- 2024/day4/solution.rs
+++ 2024/day4/solution.rs
@@ -18,7 +18,8 @@ fn _print_array(array: &Vec<Vec<char>>) {
     }
 }
 
-fn check(array: &Vec<Vec<char>>, target: [char; 4]) -> i32 {
+// check function for part 1
+fn check(array: &Vec<Vec<char>>, target: &[char]) -> i32 {
     let mut total = 0; 
     let mut row = 0;
     let mut col = 0;
@@ -26,31 +27,28 @@ fn check(array: &Vec<Vec<char>>, target: [char; 4]) -> i32 {
     while row < array.len() {
         while col < array[row].len() {
             if array[row][col] == target[0] {
-                if check_row(&array, target, row, col) {
+                if check_row(&array, &target, row, col) {
                     total += 1;                                
                 }
-                if check_col(&array, target, row, col) {
+                if check_col(&array, &target, row, col) {
                     total += 1;                                
                 }
-                if check_neg_diag(&array, target, row, col) {
+                if check_neg_diag(&array, &target, row, col) {
                     total += 1;
                 }
-                if check_pos_diag(&array, target, row, col) {
+                if check_pos_diag(&array, &target, row, col) {
                     total += 1;
                 }
             }
-            println!("{row},{col}");
             col += 1;
         }
         row += 1;
         col = 0;
     }
-
     return total;
-
 }
 
-fn check_row(array: &Vec<Vec<char>>, target: [char; 4], row: usize, col: usize) -> bool {
+fn check_row(array: &Vec<Vec<char>>, target: &[char], row: usize, col: usize) -> bool {
     for ch in 0..target.len() {
         if col + ch >= array[row].len() || target[ch] != array[row][col + ch] {
             return false;
@@ -59,7 +57,7 @@ fn check_row(array: &Vec<Vec<char>>, target: [char; 4], row: usize, col: usize) 
     return true;
 }
 
-fn check_col(array: &Vec<Vec<char>>, target: [char; 4], row: usize, col: usize) -> bool {
+fn check_col(array: &Vec<Vec<char>>, target: &[char], row: usize, col: usize) -> bool {
     for ch in 0..target.len() {
         if row + ch >= array.len() || target[ch] != array[row + ch][col] {
             return false;
@@ -68,7 +66,7 @@ fn check_col(array: &Vec<Vec<char>>, target: [char; 4], row: usize, col: usize) 
     return true;
 }
 
-fn check_neg_diag(array: &Vec<Vec<char>>, target: [char; 4], row: usize, col: usize) -> bool {
+fn check_neg_diag(array: &Vec<Vec<char>>, target: &[char], row: usize, col: usize) -> bool {
     for ch in 0..target.len() {
         if ch + row >= array.len() || ch + col >= array[row].len() || target[ch] != array[row + ch][col + ch] {
             return false;
@@ -77,7 +75,7 @@ fn check_neg_diag(array: &Vec<Vec<char>>, target: [char; 4], row: usize, col: us
     return true; 
 }
 
-fn check_pos_diag(array: &Vec<Vec<char>>, target: [char; 4], row: usize, col: usize) -> bool {
+fn check_pos_diag(array: &Vec<Vec<char>>, target: &[char], row: usize, col: usize) -> bool {
     for ch in 0..target.len() {
         if row < ch || col + ch >= array[row].len() || target[ch] != array[row - ch][col + ch] {
             return false;
@@ -87,10 +85,10 @@ fn check_pos_diag(array: &Vec<Vec<char>>, target: [char; 4], row: usize, col: us
 }
 
 fn main() {
-    let input: String = fs::read_to_string("./input.txt").expect("unable to read file");
+    let input: String = fs::read_to_string("./example.txt").expect("unable to read file");
     let input: Vec<Vec<char>> = to_array(input);
     let target: [char; 4] = ['X', 'M', 'A', 'S'];
     let rtarget: [char; 4] = ['S', 'A', 'M', 'X'];
-    let total = check(&input, target.clone()) + check(&input, rtarget.clone());
+    let total = check(&input, &target) + check(&input, &rtarget);
     println!("the total, {}", total);
 }
